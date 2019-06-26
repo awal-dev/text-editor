faab4d7ce995b9ac6d69759d28a967c8b392a7b4
Mubarak Awal <awalmubarak6@gmail.com>
2019-06-26 07:24:49 +0000

    send text value to main process

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const electron  = require('electron')
-const {app, BrowserWindow} = electron
+const {app, BrowserWindow, ipcMain} = electron
 
 app.on('ready', ()=>{
     let win = new BrowserWindow({})
     win.loadFile('index.html')
+})
+
+
+ipcMain.on('save', (event, text)=>{
+//save the text to a file
+console.log(text)
 })
